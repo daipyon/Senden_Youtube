d80be8a22e80d7c5f0840a46cfcef491074cc740
daipyon <52702633+daipyon@users.noreply.github.com>
2020-07-31 12:38:31 +0900

    Update ThisMonth_Title_Shuukei.ts

--- ThisMonth_Title_Shuukei.ts
+++ ThisMonth_Title_Shuukei.ts
@@ -27,8 +27,7 @@ const SSID = process.env.Senden_Youtube_SheetID3;
 // 今月のシートIDを取得
 let ThisMonthSheetID;
 // シート名
-// const SSName = [`News`, `公式`, `バラエティ`, `恋リア`];
-const SSName = [`バラエティ`, `恋リア`];
+const SSName = [`News`, `公式`, `バラエティ`, `恋リア`];
 // 現在のシート名
 let CurrentSSName;
 
